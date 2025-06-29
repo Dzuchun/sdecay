9b5820a7a702b236832853bed1d7db06c21c874e
Dzuchun <Infernodambik@gmail.com>
2025-06-29 08:26:50 +0300

    Various `Nuclide` methods

--- sdecay/src/wrapper/nuclide.rs
+++ sdecay/src/wrapper/nuclide.rs
@@ -5,9 +5,12 @@ use core::{
 
 use sdecay_sys::sdecay::transition_ptr_vec;
 
-use crate::wrapper::{
-    BindgenString, StdString, VecNuclideActivityPair, VecNuclideRef, VecNuclideTimeEvolution,
-    VecTransitionPtr, containers, wrapper,
+use crate::{
+    containers, wrapper,
+    wrapper::{
+        BindgenString, StdString, VecNuclideActivityPair, VecNuclideRef, VecNuclideTimeEvolution,
+        VecTransitionPtr,
+    },
 };
 
 wrapper! {
@@ -67,6 +70,141 @@ impl PartialEq for Nuclide<'_> {
             && self.isomer_number == other.isomer_number
             && self.atomic_mass == other.atomic_mass
             && self.half_life == other.half_life
+    }
+}
+
+impl Nuclide<'_> {
+    /// Checks if [`Nuclide`] has a finite half-life
+    ///
+    /// Infinite half-life implies a stable nuclide
+    #[inline]
+    pub fn is_stable(&self) -> bool {
+        let self_ptr = self.ptr();
+        // SAFETY: ffi call with
+        // - statically validated type representations
+        // - correct pointer constness (as of bindgen, that is)
+        // - `self_ptr` points to a live object, since it was just created from the reference
+        unsafe { sdecay_sys::sandia_decay::Nuclide_isStable(self_ptr) }
+    }
+
+    /// The fraction of decays of this nuclide that proceeds through the specified descendant (which may be multiple generations down the chain)
+    pub fn branching_ratio_to_descendant(&self, descendant: &Nuclide<'_>) -> f32 {
+        let self_ptr = self.ptr();
+        let descendant_ptr = descendant.ptr();
+        // SAFETY: ffi call with
+        // - statically validated type representations
+        // - correct pointer constness (as of bindgen, that is)
+        // - `self_ptr` points to a live object, since it was just created from the reference
+        unsafe {
+            sdecay_sys::sandia_decay::Nuclide_branchRatioToDecendant(self_ptr, descendant_ptr)
+        }
+    }
+
+    /// The fraction of decays of the specified ancestor (which can be multiple generations above) that proceeds through this nuclide
+    pub fn branching_ratio_from_forebear(&self, ancestor: &Nuclide<'_>) -> f32 {
+        let self_ptr = self.ptr();
+        let ancestor_ptr = ancestor.ptr();
+        // SAFETY: ffi call with
+        // - statically validated type representations
+        // - correct pointer constness (as of bindgen, that is)
+        // - `self_ptr` points to a live object, since it was just created from the reference
+        unsafe { sdecay_sys::sandia_decay::Nuclide_branchRatioFromForebear(self_ptr, ancestor_ptr) }
+    }
+
+    /// The decay constant $\lambda$ that is defined as
+    /// $$
+    /// 0.5 = \exp( - \text{decay_const} \cdot \text{half_life} )
+    /// $$
+    /// or put another way $\lambda = \ln(2)/\text{half_life}$.
+    #[expect(clippy::doc_markdown)]
+    #[inline]
+    pub fn decay_constant(&self) -> f64 {
+        let self_ptr = self.ptr();
+        // SAFETY: ffi call with
+        // - statically validated type representations
+        // - correct pointer constness (as of bindgen, that is)
+        // - `self_ptr` points to a live object, since it was just created from the reference
+        unsafe { sdecay_sys::sandia_decay::Nuclide_decayConstant(self_ptr) }
+    }
+
+    /// Maximum half-life of all nuclide descendants; dictates time-scale for secular equilibrium. If this value exceeds or equals nuclide half-life, secular equilibrium cannot be achieved, however this function will still return this value.
+    #[inline]
+    pub fn secular_equilibrium_half_life(&self) -> f64 {
+        let self_ptr = self.ptr();
+        // SAFETY: ffi call with
+        // - statically validated type representations
+        // - correct pointer constness (as of bindgen, that is)
+        // - `self_ptr` points to a live object, since it was just created from the reference
+        unsafe { sdecay_sys::sandia_decay::Nuclide_secularEquilibriumHalfLife(self_ptr) }
+    }
+
+    /// Returns true if all decendants have a shorter half life than this nuclide
+    #[inline]
+    pub fn can_obtain_secular_equilibrium(&self) -> bool {
+        let self_ptr = self.ptr();
+        // SAFETY: ffi call with
+        // - statically validated type representations
+        // - correct pointer constness (as of bindgen, that is)
+        // - `self_ptr` points to a live object, since it was just created from the reference
+        unsafe { sdecay_sys::sandia_decay::Nuclide_canObtainSecularEquilibrium(self_ptr) }
+    }
+
+    /// Prompt equilibrium half life is maximum half-life of nuclide descendants in decay series with monotonically decreasing half-lives; dictates scale for prompt equilibrium.  If prompt equilibrium half-life exceeds or equals nuclide half-life, prompt equilibrium cannot be achieved so zero is returned. For instance, an analysis application may use this to define photopeaks for `U`, `Pu`, `Th`, etc, by aging a Nuclide by $\log_2(1000)\cdot\text{promptEquilibriumHalfLife}$
+    ///
+    /// If a `promptEquilibriumHalfLife` doesn't exist, then the secular equilibrium is used, and if this doesn't exist, only for the isotope
+    #[expect(clippy::doc_markdown)]
+    #[inline]
+    pub fn prompt_equilibrium_half_life(&self) -> f64 {
+        let self_ptr = self.ptr();
+        // SAFETY: ffi call with
+        // - statically validated type representations
+        // - correct pointer constness (as of bindgen, that is)
+        // - `self_ptr` points to a live object, since it was just created from the reference
+        unsafe { sdecay_sys::sandia_decay::Nuclide_promptEquilibriumHalfLife(self_ptr) }
+    }
+
+    /// Returns true if the prompt equilibrium half life is defined
+    #[inline]
+    pub fn can_obtain_prompt_equilibrium(&self) -> bool {
+        let self_ptr = self.ptr();
+        // SAFETY: ffi call with
+        // - statically validated type representations
+        // - correct pointer constness (as of bindgen, that is)
+        // - `self_ptr` points to a live object, since it was just created from the reference
+        unsafe { sdecay_sys::sandia_decay::Nuclide_canObtainPromptEquilibrium(self_ptr) }
+    }
+
+    /// The number of atoms of this nuclide it would take to equal 1 gram
+    #[inline]
+    pub fn atoms_per_gram(&self) -> f64 {
+        let self_ptr = self.ptr();
+        // SAFETY: ffi call with
+        // - statically validated type representations
+        // - correct pointer constness (as of bindgen, that is)
+        // - `self_ptr` points to a live object, since it was just created from the reference
+        unsafe { sdecay_sys::sandia_decay::Nuclide_atomsPerGram(self_ptr) }
+    }
+
+    /// The activity (in `SandiaDecay` units) of one gram of this nuclide
+    #[inline]
+    pub fn activity_per_gram(&self) -> f64 {
+        let self_ptr = self.ptr();
+        // SAFETY: ffi call with
+        // - statically validated type representations
+        // - correct pointer constness (as of bindgen, that is)
+        // - `self_ptr` points to a live object, since it was just created from the reference
+        unsafe { sdecay_sys::sandia_decay::Nuclide_activityPerGram(self_ptr) }
+    }
+
+    /// Determine if all child isotopes are stable (e.g. aging doesn't effect gamma energies emitted)
+    #[inline]
+    pub fn decays_to_stable_children(&self) -> bool {
+        let self_ptr = self.ptr();
+        // SAFETY: ffi call with
+        // - statically validated type representations
+        // - correct pointer constness (as of bindgen, that is)
+        // - `self_ptr` points to a live object, since it was just created from the reference
+        unsafe { sdecay_sys::sandia_decay::Nuclide_decaysToStableChildren(self_ptr) }
     }
 }
 
